a314bd6556ee52c99f101826d5282b10bcf1e049
Sergio Martin <smartin@flexiant.com>
2016-02-01 17:36:15 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'helloworld'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures helloworld'
+long_description 'Installs/Configures helloworld'
+version '0.1.0'
+
+depends 'apache2'
---
+++ recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: helloworld
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'apache2::default'
+
+app_dir = "#{node['apache_test']['root_dir']}/helloworld"
+
+directory app_dir do
+  action :create
+  recursive true
+end
+
+file "#{app_dir}/index.html" do
+  content 'Hello World'
+  action :create
+end
+
+web_app 'helloworld' do
+  cookbook 'apache2'
+  server_name node['hostname']
+  server_aliases [node['fqdn']]
+  docroot app_dir
+end
